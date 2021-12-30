0f19039702a6f74fbfb8704331f26226620510a5
Miriam Farkas <miriamjoshua00@gmail.com>
2021-12-30 22:32:44 +0200

    commit tree e5889a92b229107186fd2020156a23027e88c44d

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let paymentAmount
+const feet = 500
+function scuberGreetingForFeet(feet){
+  if (feet<=400) {
+    return "This one is on me!"
+  } else if (feet>400 && feet<2000) {
+    return `I will gladly take your ${paymentAmount} bucks.`
+  } else if (feet>2000 && feet<2500) {
+    return "I will gladly take your thirty bucks."
+  }else if (feet>2500) {
+    return "No can do."
+  }
+  }
+
+let city
+
+function ternaryCheckCity(city){
+ return city === "NYC" ? "Ok, sounds good." : "No go."
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+let tipMessage
+function switchOnCharmFromTip(tipAmount){
+  switch(tipAmount) {
+    case "generous":
+      tipMessage = "Thank you so much."
+    break;
+    case "not as generous":
+      tipMessage = "Thank you."
+      break;
+    default:
+      tipMessage = "Bye."
+    }
+    return tipMessage
 }
